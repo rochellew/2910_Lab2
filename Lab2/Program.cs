6fb35ff7e4fbd9cccbba6193b98c5696461419d8
rochellew <32746298+rochellew@users.noreply.github.com>
2022-09-01 16:37:02 -0400

    Completed number 1

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// #1 print the output of adding two numbers from user input
+int num1, num2;
+Console.Write("Enter the first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{num1} + {num2} = {num1+num2}");
